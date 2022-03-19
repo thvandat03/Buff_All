aa895acaeca7b427f11648655866fd8de2114e5e
thvandat03 <95754752+thvandat03@users.noreply.github.com>
2022-03-19 13:27:34 +0700

    update fb.py

--- Facebook_clone/fb.py
+++ Facebook_clone/fb.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 # => creat a file .py
-chromeOption = webdriver.ChromeOptions()
+chromeOptions = webdriver.ChromeOptions()
 pref = {
   #load img
   'profile.managed_default_content_setting_images' : 1
 }
-chromeOption.add.add_experimental_option('pref', pref)
-driver = webdriver.Chrome('./chromedriver', Option = chromeOption)
+chromeOptions.add.add_experimental_option('pref', pref)
+driver = webdriver.Chrome('./chromedriver', options = chromeOptions)
