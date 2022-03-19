32c2952b189240285cdedeaa83d4e818cbbe9f8c
thvandat03 <95754752+thvandat03@users.noreply.github.com>
2022-03-19 12:50:28 +0700

    update gg.py

--- Google_account/gg.py
+++ Google_account/gg.py
@@ -1,12 +1,14 @@
 # install
 # pip install ipenpyxl
 import openpyxl
-
+# get value
 def get_value(fileName, cellName):
   wbk = openpyxl.load_workbook(fileName)
   Sheet1 = wbk['Sheet1']
   wbk.close()
   return Sheet1[cellName].value
+  
+# update value
 def update_value(fileName, cellName, newValue ):
   wbk = openpyxl.load_workbook(fileName)
   Sheet1 = wbk['Sheet1']
